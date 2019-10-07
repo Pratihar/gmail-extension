29a16a87df27a61ed73bc633abad3b3513dbde27
Dewanshu Pratihar <dewanshu.pratihar@gmail.com>
2019-10-07 15:02:48 +0530

    Removed unused files

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -2,7 +2,6 @@ $(window).on("load", function() {
   $(".n6")
     .find("span")
     .trigger("click");
-
   $(".wT").prepend(
     $(".aHS-aHO")
       .parent()
@@ -24,31 +23,16 @@ $(window).on("load", function() {
         .parent()
         .parent()
     );
-
   $(".aHS-bnt, .aHS-bnu, .aHS-bnq")
     .parent()
     .parent()
     .css({ marginLeft: "12px" });
-
   // Filter Trash and Spam Icons
   $(".aHS-bnx, .aHS-bnv")
     .find(".aio")
     .hide();
 
-  // Convert Icons into links
-  $(".aHS-bnx, .aHS-bnv")
-    .find(".qj")
-    .hide();
-
-  $(".aHS-bnx").prepend(
-    '<a class="qj qr" href="https://mail.google.com/mail/u/0/#trash"></a>'
-  );
-  $(".aHS-bnv").prepend(
-    '<a class="qj qr" href="https://mail.google.com/mail/u/0/#spam"></a>'
-  );
-
   $(".aHS-bnx").css({ marginLeft: "12px" });
-
   $(".aHS-bnv").css({ marginLeft: "50px", marginTop: "-32px" });
 
   var inbox = $(".aHS-bnt")
@@ -59,11 +43,9 @@ $(window).on("load", function() {
   $(".aHS-bnx").click(function() {
     window.location.href = baseurl + "#trash";
   });
-
   $(".aHS-bnv").click(function() {
     window.location.href = baseurl + "#spam";
   });
-
   $(".aHS-bnq")
     .parent()
     .parent()
@@ -80,7 +62,6 @@ $(window).on("load", function() {
         .parent()
         .parent()
     );
-
   $(".aHS-bnv:eq(1)").hide();
   $(".aHS-bnx:eq(1)").hide();
 
@@ -96,11 +77,9 @@ $(window).on("load", function() {
   $(".byl.aJZ.a0L").css({
     "box-shadow": "0px 1px 0px #000000, 0px -1px 0px #000000"
   });
-
   $(".n6")
     .find("span")
     .trigger("click");
-
   $(".J-Ke.n4").click(function() {
     $(".byl.aJZ.a0L").css({
       "border-top": "1px solid white",
@@ -117,15 +96,12 @@ $(window).on("load", function() {
   $(".aS6, .aS4, .aS3, .aS5")
     .find(".aio, .nL")
     .hide();
-
   $(".aS4")
     .find(".aHS-bnr")
     .css({ marginLeft: "60px", marginTop: "-32px" });
-
   $(".aS3")
     .find(".aHS-bnr")
     .css({ marginLeft: "110px", marginTop: "-32px" });
-
   $(".aS5")
     .find(".aHS-bnr")
     .css({ marginLeft: "160px", marginTop: "-32px" });
@@ -134,15 +110,12 @@ $(window).on("load", function() {
     $(".aS6, .aS4, .aS3, .aS5")
       .find(".aio, .nL")
       .hide();
-
     $(".aS4")
       .find(".aHS-bnr")
       .css({ marginLeft: "60px", marginTop: "-32px" });
-
     $(".aS3")
       .find(".aHS-bnr")
       .css({ marginLeft: "110px", marginTop: "-32px" });
-
     $(".aS5")
       .find(".aHS-bnr")
       .css({ marginLeft: "160px", marginTop: "-32px" });
@@ -152,7 +125,6 @@ $(window).on("load", function() {
   //   $(".aHS-bnt").toggle();
   //   $(".aHS-bnu").toggle();
   //   $(".aHS-bnq").toggle();
-
   //   if ($(this).hasClass("aii")) {
   //     $(this).removeClass("aii");
   //     $(this).addClass("aih");
@@ -162,35 +134,27 @@ $(window).on("load", function() {
   //   }
   // });
 
-  //alert($(".T-I.J-J5-Ji.aFh.T-I-ax7.mA")[0].outerHTML);
-  //alert($(".T-I.J-J5-Ji.aFh.T-I-ax7.mA")[0].outerHTML);
-
   // $(".T-Jo-auq")
   //   .find("span")
   //   .trigger("click");
-
   // $.contextMenu({
   //   selector: ".aHS-bnv, .aHS-bnx",
   //   callback: function(key, options) {
   //     var tab = "";
-
   //     if (options.$trigger.hasClass("aHS-bnv")) {
   //       tab = "spam";
   //     } else {
   //       tab = "trash";
   //     }
-
   //     popup = window.open(
   //       "https://mail.google.com/mail/u/0/#" + tab,
   //       tab,
   //       "width=800,height=500"
   //     );
-
   //     $(popup).on("load", function() {
   //       setTimeout(function() {
   //         popup.$(".x2").click();
   //       }, 3000);
-
   //       popup.setInterval(function() {
   //         if (!popup.$(".x2").length) {
   //           popup.close();
@@ -213,12 +177,5 @@ $(window).on("load", function() {
   //     // }
   //   }
   // });
-
-  // $(".aHS-bnv, .aHS-bnx").on("click", function(e) {
-  //   console.log("clicked", this);
-  // });
-
-  // $(".TN.bzz.aHS-aHO").hover(function() {
-  //   $(this).toggleClass("TO NQ");
   // });
 });
