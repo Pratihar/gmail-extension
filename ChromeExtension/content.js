e198e82deabca31f1a05713bc26c49efe1264ff4
Dewanshu Pratihar <dewanshu.pratihar@gmail.com>
2019-10-15 12:59:59 +0530

    Fixed Buttons with count

--- ChromeExtension/content.js
+++ ChromeExtension/content.js
@@ -39,43 +39,16 @@ function giveBorder() {
   });
 }
 
-$(window).on("load", function() {
-  // Trigger More Button
-  $(".n6")
-    .find("span")
-    .trigger("click");
-
-  // Set All Mail, Inbox, Sent and Drafts
-  $(".wT").prepend(
-    $(".aHS-aHO")
-      .parent()
-      .parent()
-  );
-  $(".aHS-bnt")
-    .parent()
-    .parent()
-    .after(
-      $(".aHS-bnu")
-        .parent()
-        .parent()
-    );
-  $(".aHS-bnu")
-    .parent()
-    .parent()
-    .after(
-      $(".aHS-bnq")
-        .parent()
-        .parent()
-    );
-
-  // Give Margin to Inbox, Sent and Drafts
-  $(".byl").css({ marginLeft: "12px" });
-
+function positionSpam() {
   // Extract Trash and Spam Icons from Buttons
   $(".aHS-bnx, .aHS-bnv")
     .find(".aio")
     .find("span")
     .find("a")
+    .html("&nbsp;&emsp;");
+  $(".aHS-bnv")
+    .find(".aio")
+    .find(".bsU")
     .html("&nbsp;&emsp;");
 
   // Trash and Spam Button Routing
@@ -111,10 +84,45 @@ $(window).on("load", function() {
         .parent()
         .parent()
     );
+}
 
-  // Hide Trash and Spam from More Section
-  $(".aHS-bnv:eq(1)").hide();
-  $(".aHS-bnx:eq(1)").hide();
+function mutation(target) {}
+
+$(window).on("load", function() {
+  // Trigger More Button
+  $(".n6")
+    .find("span")
+    .trigger("click");
+
+  gmail = Gmail();
+
+  // Set All Mail, Inbox, Sent and Drafts
+  $(".wT").prepend(
+    $(".aHS-aHO")
+      .parent()
+      .parent()
+  );
+  $(".aHS-bnt")
+    .parent()
+    .parent()
+    .after(
+      $(".aHS-bnu")
+        .parent()
+        .parent()
+    );
+  $(".aHS-bnu")
+    .parent()
+    .parent()
+    .after(
+      $(".aHS-bnq")
+        .parent()
+        .parent()
+    );
+
+  // Give Margin to Inbox, Sent and Drafts
+  $(".byl").css({ marginLeft: "12px" });
+
+  positionSpam();
 
   // var arrow = $(".TH.aii.J-J5-Ji");
   // $(".aHS-aHO").prepend(arrow);
@@ -165,9 +173,30 @@ $(window).on("load", function() {
     resizeResponse();
   });
 
-  $(document).arrive(".aS6", function() {
-    resizeResponse();
+  var fake = $(".TK").clone();
+
+  // select the target node
+  var target = $(".TK")[0];
+
+  // configuration of the observer:
+  var config = { attributes: true, childList: true, characterData: true };
+
+  // create an observer instance
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      console.log("change");
+      // $("<div>" + fake + "</div>")
+      //   .find(".bsU:eq(1)")
+      //   .text(gmail.get.unread_draft_emails());
+      $(".TK")[0].replaceWith(fake[0]);
+      fake = $(".TK").clone();
+      target = $(".TK")[0];
+      observer.observe(target, config);
+    });
   });
+
+  // pass in the target node, as well as the observer options
+  observer.observe(target, config);
 
   // arrow.click(function() {
   //   $(".aHS-bnt").toggle();
